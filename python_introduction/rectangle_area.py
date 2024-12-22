61bb906694e1e24c36422de3a131f04bcb633fb4
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2024-12-23 00:18:34 +0200

    Rectangle Area

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+#  Define Two Variables
+length = 10
+width = 5
+
+# Calculate the Area (Area= length * width )
+area = length * width
+
+# Print the Area
+print(f"The area of the rectangle is:{area}")
